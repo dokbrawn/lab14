32ca4a144303c6013e161c1d22c5e13779a2a6b7
Док Браун <tanisimov2006@gmail.com>
2025-05-26 10:05:40 +0700

    Initial commit

--- js/main.js
+++ js/main.js
@@ -1,24 +1,47 @@
-import '../css/style.css'
-import javascriptLogo from '../javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import html2pdf from 'html2pdf.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+document.addEventListener('DOMContentLoaded', () => {
+  const button = document.getElementById('download')
+  const resume = document.getElementById('resume')
 
-setupCounter(document.querySelector('#counter'))
+  if (button && resume) {
+    button.addEventListener('click', () => {
+      const clone = resume.cloneNode(true)
+      clone.querySelectorAll('[contenteditable]').forEach(el => el.removeAttribute('contenteditable'))
+      clone.style.animation = 'none'
+      clone.style.boxShadow = 'none'
+
+      const wrapper = document.createElement('div')
+      wrapper.style.position = 'absolute'
+      wrapper.style.left = '-9999px'
+      wrapper.appendChild(clone)
+      document.body.appendChild(wrapper)
+
+      html2pdf()
+        .set({
+          margin: 0.5,
+          filename: 'resume.pdf',
+          html2canvas: { scale: 2, useCORS: true },
+          jsPDF: { unit: 'in', format: 'a4', orientation: 'portrait' }
+        })
+        .from(clone)
+        .save()
+        .then(() => {
+          wrapper.remove()
+        })
+    })
+
+    button.addEventListener('click', function (e) {
+      const ripple = document.createElement('span')
+      ripple.classList.add('ripple')
+
+      const rect = this.getBoundingClientRect()
+      ripple.style.left = `${e.clientX - rect.left}px`
+      ripple.style.top = `${e.clientY - rect.top}px`
+      ripple.style.width = ripple.style.height = '100px'
+
+      this.appendChild(ripple)
+      setTimeout(() => ripple.remove(), 600)
+    })
+  }
+})
